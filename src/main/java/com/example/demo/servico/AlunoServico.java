418e0b08adc5c70de3e99192ec3052b5659ae3f3
Josias Henrique <joystickhenrii@gmail.com>
2021-08-11 20:53:26 -0300

    Adicionando comentarios no CRUD

--- src/main/java/com/example/demo/servico/AlunoServico.java
+++ src/main/java/com/example/demo/servico/AlunoServico.java
@@ -1,5 +1,6 @@
 package com.example.demo.servico;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.dominio.Aluno;
 import com.example.demo.repositorios.AlunoRepositorio;
-
 import com.example.demo.servico.excecao.ObjectNotFoundException;
 
 @Service
@@ -15,10 +15,27 @@ public class AlunoServico {
 
 	@Autowired
 	private AlunoRepositorio repo;
-	
+
 	public Aluno buscar(Integer id) {
 		Optional<Aluno> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Aluno.class.getName()));
 	}
+
+	public Aluno insert(Aluno obj) {
+		obj.setId(null);
+		return obj = repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		buscar(id);
+
+		repo.deleteById(id);
+
+	}
+
+	public List<Aluno> findAll() {
+		return repo.findAll();
+	}
+
 }
